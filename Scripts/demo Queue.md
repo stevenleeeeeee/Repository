33d62901fecf784cb82320d3e24dc0b7c2690984
Bluevitality <inmoonlight@163.com>
2019-08-17 16:03:15 +0800

    python学习笔记修改

--- Scripts/demo Queue.md
+++ Scripts/demo Queue.md
@@ -1,10 +1,9 @@
-#### 简介
-```txt
+```
 Queue是python标准库中的线程安全的队列（FIFO）实现，提供了一个适用于多线程编程的先进先出的数据结构，即队列
 用来在生产者和消费者线程之间的信息传递
 ```
 
-#### 基本FIFO队列
+#### First in First Out
 ```python
 #FIFO即First in First Out,先进先出。Queue提供了一个基本的FIFO容器
 #使用方法很简单,maxsize是个整数，指明了队列中能存放的数据的上限。一旦达到上限则插入会导致阻塞，直到队列中的数据被消费掉
@@ -25,7 +24,7 @@ Queue是python标准库中的线程安全的队列（FIFO）实现，提供了�
 4
 ```
 
-#### LIFO队列
+#### Last in First Out
 ```python
 #LIFO即Last in First Out,后进先出。与栈的类似，使用也简单，maxsize用法同上
 
@@ -60,6 +59,7 @@ Queue是python标准库中的线程安全的队列（FIFO）实现，提供了�
 ...         print 'Job:',description
 ...         return
 ...     def __cmp__(self, other):
+...         # 当队列的元素是自定义时，需要在元素的类中定义这些队列中的元素的比较规则
 ...         return cmp(self.priority, other.priority)
 ... 
 >>> q = Queue.PriorityQueue()
@@ -86,7 +86,7 @@ for: level 3 job
 for: level 10 job
 ```
 
-#### 一些常用方法
+#### 常用方法
 ```txt
 task_done()
 意味着之前入队的一个任务已经完成。由队列的消费者线程调用
@@ -94,9 +94,9 @@ task_done()
 如果当前一个join()在阻塞，它将在队列中所有任务都处理完时恢复执行（即每个由put()调用入队的任务都有对应的task_done()调用）
 
 join()
-阻塞调用线程，直到队列中的所有任务被处理掉。
-只要有数据被加入队列未完成的任务数就会增加。当消费者线程调用task_done()（意味着消费者取得任务并完成）未完成的任务数就会减少
-当未完成的任务数降到0，join()解除阻塞。(join实际上意味着等到队列为空，再执行别的操作)
+阻塞调用线程，直到队列中的所有任务被处理掉。（实际上意味着等到队列为空，再执行别的操作）
+只要有数据被加入队列未完成的任务数就会增加。当消费者线程调用task_done()（意味着消费者取得任务并完成）未完成的任务数即减少
+当未完成的任务数降到0时join()解除阻塞。
 
 put(item[, block[, timeout]])
 将item放入队列
@@ -104,7 +104,6 @@ put(item[, block[, timeout]])
 如果timeout是个正整数，阻塞调用进程最多timeout秒，如果一直无空空间可用，抛出Full异常（带超时的阻塞调用）
 如果block为False，如果有空闲空间可用将数据放入队列，否则立即抛出Full异常
 其非阻塞版本为put_nowait等同于put(item, False)
-
 
 get([block[, timeout]])
 从队列中移除并返回一个数据。block跟timeout参数同put方法
--- Scripts/demo subprocess.py
+++ Scripts/demo subprocess.py
@@ -1,33 +1,32 @@
 """
-subprocess从2.4版本引入。主要用来取代一些旧模块如 os.system、os.spawn*、os.popen*、commands.* ...
-其不但可调用外部命令作为子进程，而且可以连接到子进程的 input/output/error 管道以获取相关返回信息
-通过标准库中的subprocess包来fork子进程并运行一个外部程序（其定义了数个创建子进程的函数，它们分别以不同方式创建子进程）
-"""
+subprocess从2.4版本引入。用来取代一些旧模块如 os.system、os.spawn*、os.popen*、commands.* ...
+其不但可调用外部命令作为子进程，还可连接到子进程的 input/output/error 管道以获取相关返回信息
+通过标准库中的subprocess包来fork子进程并运行一个外部程序（其定义了数个创建子进程的函数，分别以不同方式创建子进程）
 
-#参数说明：
-args:         #可以是一个字符串，可以是一个包含程序参数的列表。要执行的程序一般就是这个列表的第一项，或者是字符串本
+# 参数说明：
+args:         #可以是字符串，可以是包含程序参数的列表。要执行的程序一般就是这个列表的第一项，或者是字符串本
 executable:   #一般不用，args字符串或列表第一项表示程序名
 stdin/stdout/stderr:
-              #None 表示没有任何重定向，继承父进程
-              #PIPE 创建管道
-              #文件对象
-              #文件描述符(整数)
-preexec_fn    #钩子函数，在fork和exec之间执行(Unix)
-close_fds     #在Unix下表示在执行新进程之前是否关闭0/1/2之外的文件，在windows下为是否继承父进程的文件描述符
-shell         #为真时在Unix下相当于args前面添加了：/bin/sh -c，Windows下相当于添加了：cmd.exe /c
-cwd           #设置工作目录 ( 如果cwd不是None，则会把cwd做为子程序的当前目录 )
-env           #设置环境变量 ( 如果env不是None，则子程序的环境变量由env的值来设置 )
-
+              # None 表示没有任何重定向，继承父进程
+              # PIPE 创建管道
+              # 文件对象
+              # 文件描述符(整数)
+preexec_fn    # 钩子函数，在fork和exec之间执行(Unix)
+close_fds     # 在Unix下表示在执行新进程之前是否关闭0/1/2之外的文件，在windows下为是否继承父进程的文件描述符
+shell         # 为真时在Unix下相当于args前面添加了：/bin/sh -c，Windows下相当于添加了：cmd.exe /c
+cwd           # 设置工作目录 ( 如果cwd不是None，则会把cwd做为子程序的当前目录 )
+env           # 设置环境变量 ( 如果env不是None，则子程序的环境变量由env的值来设置 )
+"""
 
 # demo1
 import subprocess
 """
 在复杂场景中需将一个进程的执行输出作为另一个进程的输入。
-在另一些场景中我们需要先进入到某个输入环境然后再执行一系列的指令等。这时要用 Popen()
-参数：
+在另一些场景中需要先进入到某个输入环境然后再执行一系列的指令等。这时要用 Popen()
+参数说明：
 args：                 shell命令，可以是字串或序列类型如 list,tuple。
 bufsize：              缓冲区大小，可不关心
-stdin,stdout,stderr：  分别表示程序的标准输入，标准输出，标准错误
+stdin/stdout/stderr:   分别表示程序的标准输入，标准输出，标准错误
 shell：                与下面方法中用法相同
 cwd：                  设置子进程的当前目录
 env：                  指定子进程的环境变量。若 env=None 则默认从父进程继承环境变量
@@ -49,7 +48,9 @@ s.stderr.close()
 # demo2
 import subprocess
 """
-subprocess.call()：执行命令并返回状态，其中shell参数为False时，命令需要通过列表方式传入，当shell为True时可直接传入命令
+subprocess.call()：执行并返回状态，其shell参数为False时命令需要通过列表方式传入，当shell为True时可直接传入命令
+subprocess.check_call()与subprocess.call()相同，唯一区别是当返回值不为 0 时直接抛出异常
+subprocess.check_output()与上面两个方法类似，区别是如果当返回值为0时直接返回输出结果，不为0则抛出异常（仅在python3中）
 """
 >>> a = subprocess.call(['df','-hT'],shell=False)
 Filesystem    Type    Size  Used Avail Use% Mounted on
@@ -63,31 +64,9 @@ Filesystem    Type    Size  Used Avail Use% Mounted on
 tmpfs        tmpfs    2.8G     0  2.8G   0% /dev/shm
 /dev/sda1     ext4    976M   56M  853M   7% /boot
 
->>> print a
-0
-
 
 # demo3
-import subprocess
-"""
-subprocess.check_call()：与subprocess.call()类似，区别是当返回值不为0时直接抛出异常
-"""
->>> a = subprocess.check_call('dfdsf',shell=True)
-/bin/sh: dfdsf: command not found
-Traceback (most recent call last):
-  File "<stdin>", line 1, in <module>
-  File "/usr/lib64/python2.6/subprocess.py", line 502, in check_call
-    raise CalledProcessError(retcode, cmd)
-subprocess.CalledProcessError: Command 'dfdsf' returned non-zero exit status 127
-
-
-#注：
-# subprocess.check_output() 与上面两个方法类似，区别是如果当返回值为0时直接返回输出结果，不为0则抛出异常
-# 该方法在python3.x中才有
-
-
-# demo4
-#将一个子进程的输出，作为另一个子进程的输入
+# 将一个子进程的输出，作为另一个子进程的输入
 import subprocess
 child1 = subprocess.Popen(["cat","/etc/passwd"], stdout=subprocess.PIPE)
 child2 = subprocess.Popen(["grep","0:0"],stdin=child1.stdout, stdout=subprocess.PIPE)
@@ -97,11 +76,45 @@ out = child2.communicate()
 # Other
 import subprocess
 child = subprocess.Popen('sleep 60',shell=True,stdout=subprocess.PIPE)
-child.poll()          #检查子进程状态
-child.kill()          #终止子进程
-child.send_signal()   #向子进程发送信号
-child.terminate()     #终止子进程
-
+child.poll()            # 检查子进程是否结束，并返回returncode属性（若没有接数则返回None）
+child.wait()            # 等待子进程执行结束，并返回returncode属性，如果为0表示执行成功（中间会一直阻塞等待）
+child.kill()            # 终止子进程
+child.send_signal(sig)  # 向子进程发送信号
+child.terminate()       # 终止子进程，还有 child.kill() 作用与其相同
+child.pid               # PID
 
 # 通过对subprocess.Popen的封装来实现的高级函数:
 # subprocess.run()、subprocess.call()、subprocess.check_call()和subprocess.check_output()
+
+
+# demo4
+import subprocess
+
+class Shell(object):
+
+    def runCmd(self, cmd) :
+        res = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        sout ,serr = res.communicate()  
+
+    return res.returncode, sout, serr, res.pid
+
+shell = Shell()
+
+while 1 :
+    input = raw_input('>')
+    if input == 'exit' or input == 'bye' :
+        break
+    else :
+        result = shell.runCmd(input)
+        print "返回码：", result[0]
+        print "标准输出：", result[1]
+        print "标准错误：", result[2]
+
+
+# subprocess.getstatusoutput()
+# 接受字串形式命令，返回元组形式的结果，第一个元素是命令执行状态，第二个为执行结果
+# subprocess.getoutput()
+# 接受字符串形式的命令，返回执行结果
+>>> subprocess.getstatusoutput('pwd')
+(0, '/root')
+
